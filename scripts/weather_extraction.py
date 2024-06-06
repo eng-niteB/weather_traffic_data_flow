47fa07b6a5493bf99b530b1bd78e54b91f7eb1f5
Roberto Rocha <rb.rochap@gmail.com>
2024-06-06 14:27:24 +0000

    FIX: corrigindo a funcao create_table para criar a tabela, ajustando o main e testando a criacao

--- scripts/weather_extraction.py
+++ scripts/weather_extraction.py
@@ -4,6 +4,7 @@ import requests
 from pyspark.sql import SparkSession
 from typing import Dict, Any, List
 from pyspark.sql import types as T
+from pyspark.sql import DataFrame
 
 # Adicionar o diretório principal ao sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -14,7 +15,7 @@ from utils.timer import timer_func
 load_env_variables()
 
 @timer_func
-def get_spark_session(temp_dir: str) -> SparkSession:
+def get_spark_session(jar_path: str, temp_dir: str) -> SparkSession:
     """
     Retorna a sessao de spark configurada
     """
@@ -22,6 +23,11 @@ def get_spark_session(temp_dir: str) -> SparkSession:
         .appName("ETLJob") \
         .config("spark.jars", jar_path) \
         .config("spark.local.dir", temp_dir) \
+        .config("spark.sql.warehouse.dir", "/tmp/warehouse") \
+        .config("spark.hadoop.fs.file.impl.disable.cache", "true") \
+        .config("spark.hadoop.fs.hdfs.impl.disable.cache", "true") \
+        .config("spark.hadoop.fs.s3a.impl.disable.cache", "true") \
+        .config("spark.hadoop.fs.nativeio.impl.disable.cache", "true") \
         .master("local[*]") \
         .getOrCreate()
     
@@ -59,16 +65,6 @@ def get_weather_data(citys: List[str]) -> List[Dict[str, Any]]:
     return [weather, errors]
 
 @timer_func
-def create_raw_table(spark : SparkSession,dir: str, table_name: str, schema : T.StructType, partitionKey : str) -> None:
-    empty_df = spark.createDataFrame([], schema)
-    
-    print(empty_df.show())
-    
-    empty_df.write.partitionBy(partitionKey).parquet(dir)
-    
-    spark.stop()
-    
-@timer_func
 def get_weather_schema() -> T.StructType:
     """
     Descreve e retorna a estrutura de dados esperada para a tabela de clima
@@ -100,13 +96,16 @@ def get_weather_schema() -> T.StructType:
     
     return schema
 
-if __name__ == "__main__":
+@timer_func
+def create_table(spark : SparkSession,dir: str, table_name: str, schema : T.StructType, partitionKey : str) -> None:
+    #Criando dados da carga inicial
+    num_columns = len(schema.fields)
+    initial_data = [(None,) * num_columns]
     
-    # Define os caminhos para o executavel Python (Segundo os foruns um dos problemas cronicos do spark com o windows)
-    python_path = "C:\\Python311\\python.exe"
-    os.environ["PYSPARK_PYTHON"] = python_path
-    os.environ["PYSPARK_DRIVER_PYTHON"] = python_path
-    
+    empty_df = spark.createDataFrame(initial_data, schema)    
+    empty_df.write.partitionBy(partitionKey).parquet(dir)
+
+if __name__ == "__main__":    
     citys = ['Divinopolis']
     schema = 'raw'
     table_name = 'weather_data'
@@ -116,13 +115,18 @@ if __name__ == "__main__":
     jar_path = os.getenv('POSTGRES_JAR_PATH')
     database_dir = os.getenv('DATABASE_DIR')
     
-    raw_dir = f"{database_dir}\\\\raw\\\\{table_name}"
+    schema_dir = f"{database_dir}/{schema}"
+    table_dir = f"{schema_dir}/{table_name}"
     partitionKey = "dt"
-    spark = get_spark_session(temp_dir)
+    spark = get_spark_session(jar_path,temp_dir)
     
-    weather_scema = get_weather_schema()
+    weather_schema = get_weather_schema()
     
-    create_raw_table(spark,raw_dir,table_name,weather_scema,partitionKey)
+    #create_table(spark,table_dir,table_name,weather_schema,partitionKey)
+    
+    df = spark.read.parquet(table_dir)
+    print(df.show())
+    spark.stop()
     
     
 
