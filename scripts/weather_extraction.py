6ef5c6666fb27bc1398cf11d50883353c4a674a9
eng-niteB <rb.rochap@gmail.com>
2024-06-06 08:34:16 -0300

    FEAT: Adicionando funcao que cria a estrutura da tabela vazia no ambiente local

--- scripts/weather_extraction.py
+++ scripts/weather_extraction.py
@@ -3,6 +3,7 @@ import sys
 import requests
 from pyspark.sql import SparkSession
 from typing import Dict, Any, List
+from pyspark.sql import types as T
 
 # Adicionar o diretório principal ao sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -56,6 +57,48 @@ def get_weather_data(citys: List[str]) -> List[Dict[str, Any]]:
             
     return [weather, errors]
 
+@timer_func
+def create_raw_table(spark : SparkSession,dir: str, table_name: str, schema : T.StructType, partitionKey : str) -> None:
+    empty_df = spark.createDataFrame([], schema)
+    
+    print(empty_df.show())
+    
+    empty_df.write.partitionBy(partitionKey).parquet(dir)
+    
+    spark.stop()
+    
+@timer_func
+def get_weather_schema() -> T.StructType:
+    """
+    Descreve e retorna a estrutura de dados esperada para a tabela de clima
+    """
+    schema = T.StructType([
+            T.StructField("id", T.IntegerType(), True),
+            T.StructField("city", T.StringType(), True),
+            T.StructField("country", T.StringType(), True),
+            T.StructField("lon", T.FloatType(), True),
+            T.StructField("lat", T.FloatType(), True),
+            T.StructField("weather_description", T.StringType(), True),
+            T.StructField("temp_f", T.FloatType(), True),
+            T.StructField("feels_like_f", T.FloatType(), True),
+            T.StructField("temp_min_f", T.FloatType(), True),
+            T.StructField("temp_max_f", T.FloatType(), True),
+            T.StructField("pressure", T.IntegerType(), True),
+            T.StructField("humidity", T.IntegerType(), True),
+            T.StructField("sea_level", T.IntegerType(), True),
+            T.StructField("grnd_level", T.IntegerType(), True),
+            T.StructField("visibility", T.IntegerType(), True),
+            T.StructField("wind_speed", T.FloatType(), True),
+            T.StructField("wind_deg", T.IntegerType(), True),
+            T.StructField("wind_gust", T.FloatType(), True),
+            T.StructField("sunrise", T.LongType(), True),
+            T.StructField("sunset", T.LongType(), True),
+            T.StructField("load_dt", T.LongType(), True),
+            T.StructField("dt", T.StringType(), True)
+        ])
+    
+    return schema
+
 if __name__ == "__main__":
     citys = ['Divinopolis']
     schema = 'raw'
@@ -64,13 +107,15 @@ if __name__ == "__main__":
     password = os.getenv('POSTGRES_PASSWORD')
     temp_dir = os.getenv('TEMP_DIR')
     jar_path = os.getenv('POSTGRES_JAR_PATH')
-        
-    api_data = get_weather_data(citys)
-    weather = api_data[0]
-    errors = api_data[1]
+    database_dir = os.getenv('DATABASE_DIR')
     
-    print(f"""
-          weather_data : {weather}
-          errors: {errors}
-          """)
+    raw_dir = f"{database_dir}\\\\raw"
+    partitionKey = "dt"
+    spark = get_spark_session(temp_dir)
+    
+    weather_scema = get_weather_schema()
+    
+    create_raw_table(spark,raw_dir,table_name,weather_scema,partitionKey)
+    
+    
 
