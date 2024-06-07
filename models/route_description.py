47ac51e69afd9b94945e78e3db4205f254f077c3
Roberto Rocha <rb.rochap@gmail.com>
2024-06-07 21:09:40 +0000

    FEAT: Finalizada a primeira versao do tb_descricao_rota e feito teste de insercao

---
+++ models/route_description.py
@@ -0,0 +1,112 @@
+import os
+import sys
+from pyspark.sql import types as T
+from typing import Dict, Any, List
+from datetime import datetime
+from dataclasses import dataclass
+from pyspark.sql import SparkSession
+
+# Adicionar o diretório principal ao sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils.config import timer_func, load_env_variables
+from utils.spark import get_citys_data
+load_env_variables()
+
+@dataclass(frozen=True)
+class routeDescription:
+    trusted_schema: str = 'trusted'
+    trusted_name: str = 'descricao_rota'
+    trusted_key_column: str = 'nu_rota'
+    trusted_order_column: str = "dt_carga"
+    
+    @staticmethod
+    def get_trusted_schema() -> T.StructType:
+        """
+        Descreve e retorna a estrutura de dados esperada para a tabela de tráfego na trusted zone
+        
+        Retorno:
+        StructType: Estrutura que será utilizada para montar o DataFrame
+        """
+        schema = T.StructType([
+                T.StructField("nu_rota", T.StringType(), True),
+                T.StructField("nu_origem", T.IntegerType(), True),
+                T.StructField("origem", T.StringType(), True),
+                T.StructField("ponto_partida", T.StringType(), True),
+                T.StructField("descricao_tempo_origem", T.StringType(), True),
+                T.StructField("temp_origem", T.FloatType(), True),
+                T.StructField("sensacao_termica_origem", T.FloatType(), True),
+                T.StructField("temp_minima_origem", T.FloatType(), True),
+                T.StructField("temp_max_origem", T.FloatType(), True),
+                T.StructField("altitude_origem", T.IntegerType(), True),
+                T.StructField("nu_destino", T.IntegerType(), True),
+                T.StructField("destino", T.StringType(), True),  # Corrigido de "origem" para "destino"
+                T.StructField("ponto_chegada", T.StringType(), True),
+                T.StructField("descricao_tempo_destino", T.StringType(), True),
+                T.StructField("temp_destino", T.FloatType(), True),
+                T.StructField("sensacao_termica_destino", T.FloatType(), True),
+                T.StructField("temp_minima_destino", T.FloatType(), True),
+                T.StructField("temp_max_destino", T.FloatType(), True),
+                T.StructField("altitude_destino", T.IntegerType(), True),
+                T.StructField("passos", T.ArrayType(T.MapType(T.StringType(), T.StringType())), True),
+                T.StructField("dt_carga", T.LongType(), True),
+                T.StructField("dt", T.StringType(), True)
+            ])
+        
+        return schema
+    
+    @staticmethod
+    @timer_func
+    def format_data(spark : SparkSession, weather_table_dir : str, datas: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        """
+        Formata os Jsons retornados pela API para a estrutura esperada do DataFrame
+        
+        Argumentos:
+        spark (SparkSession): Sessão do spark utilizada nos módulos
+        weather_table_dir (str): Caminho da tabela com os dados de cidades
+        datas (List[Dict[str, Any]]): Rotas existentes na tabela dados_trafego
+        
+        Retorno:
+        List[Dict[str, Any]]: Json formatado já seguindo a estrutura padrão da tabela e com a nova coluna criada
+        """
+        new_data = []
+        
+        for data in datas:
+            origin = data['cidade_origem']
+            destination = data['cidade_destino']
+            ts = int(datetime.timestamp(datetime.now()))
+            dt = data['dt']
+            
+            citys_data = get_citys_data(spark,weather_table_dir,origin,destination,dt)
+            
+            origin_data = citys_data[0]
+            destination_data = citys_data[1]
+            
+            formatted_data = {
+                "nu_rota" : data['nu_rota'],
+                "nu_origem" : data["nu_cidade_origem"],
+                "origem" : data["cidade_origem"],
+                "ponto_partida" : data['localizacao_inicial'],
+                "descricao_tempo_origem" : origin_data['descricao_tempo'],
+                "temp_origem" : origin_data['temperatura'],
+                "sensacao_termica_origem" : origin_data['sensacao_termica'],
+                "temp_minima_origem" : origin_data['temperatura_minima'],
+                "temp_max_origem" : origin_data['temperatura_maxima'],
+                "altitude_origem" : origin_data.get('nivel_do_mar'),
+                "nu_destino" : data["nu_cidade_destino"],
+                "destino" : data["cidade_destino"],
+                "ponto_chegada" : data['localizacao_final'],
+                "descricao_tempo_destino" : destination_data['descricao_tempo'],
+                "temp_destino" : destination_data['temperatura'],
+                "sensacao_termica_destino" : destination_data['sensacao_termica'],
+                "temp_minima_destino" : destination_data['temperatura_minima'],
+                "temp_max_destino" : destination_data['temperatura_maxima'],
+                "altitude_destino" : destination_data.get('nivel_do_mar'),
+                "passos" : data['passos'],
+                "dt_carga" : ts,
+                "dt" : data["dt"]
+            }
+            
+            new_data.append(formatted_data)
+    
+        return new_data
--- scripts/tb_descricao_rota.py
+++ scripts/tb_descricao_rota.py
@@ -1,13 +1,60 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+from datetime import datetime
+from typing import Dict, Any, List
 
 #Adicionar o diretório principal ao sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from utils.config import timer_func,load_env_variables
+from utils.spark import get_spark_session,create_table,insert_trusted_data
+from models.traffic import Traffic
+from models.weather import Weather
+from models.route_description import routeDescription
 
 load_env_variables()
 
 if __name__ == "__main__":
-    print('Hello')
+    #Buscando o caminho do diretorio base das tabelas
+    database_dir : str = os.getenv('DATABASE_DIR')
+    
+    #Criando as classes a serem utilizdas
+    weather : Weather = Weather()
+    routes : Traffic = Traffic()
+    route_description = routeDescription()
+    
+    key_column = route_description.trusted_key_column
+    order_colum = route_description.trusted_order_column
+    
+    #Montando caminhos especificos da camda e da tabela
+    routes_table_dir : str = f"{database_dir}/{routes.trusted_schema}/{routes.trusted_name}"
+    weather_table_dir : str = f"{database_dir}/{weather.trusted_schema}/{weather.trusted_name}"
+    route_description_table_dir : str = f"{database_dir}/{route_description.trusted_schema}/{route_description.trusted_name}"
+    
+    #Criando sessão spark
+    spark =  get_spark_session()
+     
+    #Verificando se a tabela existe e se não criando-a
+    create_table(spark,route_description_table_dir,route_description.trusted_name,route_description.get_trusted_schema())
+    
+    df_routes = spark.read.parquet(routes_table_dir)
+    
+    routes = df_routes.collect()
+    
+    datas : List[Dict[str, Any]] = [row.asDict() for row in routes]
+     
+    new_data = route_description.format_data(spark,weather_table_dir,datas)
+    
+    df_new_data = spark.createDataFrame(new_data,route_description.get_trusted_schema())
+    
+    df_trusted = spark.read.parquet(route_description_table_dir)
+    
+    insert_trusted_data(df_new_data, df_trusted, key_column, order_colum, route_description_table_dir)
+    
+    spark.stop()
+    
+    
+        
+    
+        
