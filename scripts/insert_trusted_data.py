582f5f6b606c9e4edd1bc25d4a82be369b4e2920
Roberto Rocha <rb.rochap@gmail.com>
2024-06-10 12:19:38 +0000

    FEAT: Inicializando python unico para camada da trusted insert_trusted_data.py

---
+++ scripts/insert_trusted_data.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+#Adicionar o diretório principal ao sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+if __name__ == "__main__":
+    print("Hello")
